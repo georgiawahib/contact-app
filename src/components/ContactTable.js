ce8cbbf60ff84f8ab97f870b560993354943c14c
Georgia Wahib <z5285348@unsw.edu.au>
2022-04-13 18:31:37 +1000

    SCROLLY GRID

---
+++ src/components/ContactTable.js
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react";
+import ContactCard from "./ContactCard";
+import axios from "axios";
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+
+function ContactTable() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [allUsers, setAllUsers] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        setAllUsers(res.data);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }, []);
+
+  return (
+    <>
+      {!isLoading && (
+        <Box sx={{ height: "80vh", overflow: "auto" }}>
+          <Grid
+            container
+            spacing={3}
+            justifyContent="space-around"
+            columns={{ xs: 4, sm: 8, md: 12 }}
+          >
+            {allUsers.map((user, index) => {
+              return (
+                <Grid item>
+                  <ContactCard user={user} />
+                </Grid>
+              );
+            })}
+          </Grid>
+        </Box>
+      )}
+    </>
+  );
+}
+
+export default ContactTable;
---
+++ src/components/Heading.js
@@ -0,0 +1,14 @@
+import * as React from "react";
+import Typography from "@mui/material/Typography";
+
+function Heading() {
+  return (
+    <>
+      <Typography variant="h3" gutterBottom component="div">
+        Contacts
+      </Typography>
+    </>
+  );
+}
+
+export default Heading;
